4e678cfd19876c2ab2265160ff31dc9e016548c6
inesmaga <inesmaga@gmail.com>
2018-03-28 10:08:50 +0200

    subida 28/03

---
+++ build/generated-sources/ap-source-output/wartokg/entity/wartokgUser_.java
@@ -0,0 +1,25 @@
+package wartokg.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-28T06:05:12")
+@StaticMetamodel(wartokgUser.class)
+public class wartokgUser_ { 
+
+    public static volatile SingularAttribute<wartokgUser, Double> TMBpi;
+    public static volatile SingularAttribute<wartokgUser, String> Valorac;
+    public static volatile SingularAttribute<wartokgUser, Double> PesoId;
+    public static volatile SingularAttribute<wartokgUser, Double> FAf;
+    public static volatile SingularAttribute<wartokgUser, Integer> edad;
+    public static volatile SingularAttribute<wartokgUser, Integer> altura;
+    public static volatile SingularAttribute<wartokgUser, Double> TMBpa;
+    public static volatile SingularAttribute<wartokgUser, Double> KcalMP;
+    public static volatile SingularAttribute<wartokgUser, Double> KcalAd;
+    public static volatile SingularAttribute<wartokgUser, Long> id;
+    public static volatile SingularAttribute<wartokgUser, Double> PesoAc;
+    public static volatile SingularAttribute<wartokgUser, String> sexo;
+    public static volatile SingularAttribute<wartokgUser, Double> IMC;
+
+}
