1b5f14fbece448c2fe6e89d82785c570be793c97
inesmaga <inesmaga@gmail.com>
2018-04-02 12:02:12 +0200

    subida con lanzamiento3

--- build/generated-sources/ap-source-output/wartokg/entity/wartokgUser_.java
+++ build/generated-sources/ap-source-output/wartokg/entity/wartokgUser_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-02T11:38:29")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-02T11:59:33")
 @StaticMetamodel(wartokgUser.class)
 public class wartokgUser_ { 
 
